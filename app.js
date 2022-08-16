5594fc99453b2b6fb1f5c0c1457433227e8d37ae
Karabo <karaborabothata0@gmail.com>
2022-08-16 21:21:45 +0200

    accept number input from dom

--- app.js
+++ app.js
@@ -3,15 +3,27 @@ const subtractButton = document.getElementById("subtractButton");
 const multiplyButton = document.getElementById("multiplyButton");
 const divideButton = document.getElementById("divideButton");
 const calculateButton = document.getElementById("calculateButton");
+const numberCollection = document.getElementById("numberCollection").childNodes;
 
 let numberStore = [];
 
 function setOperatorEventListener(...buttons) {
 	buttons.forEach((button) => {
-		button.addEventListener("click", addOperatorToNumber);
+		button.addEventListener("click", pushOperator);
 	});
 }
 
+function setNumberEventListener(numbers) {
+	numbers.forEach((number) => {
+		number.addEventListener("click", (e) => {
+			e.preventDefault();
+			pushNumber(e);
+		});
+	});
+}
+
+calculateButton.addEventListener("click", calculate);
+setNumberEventListener(numberCollection);
 setOperatorEventListener(
 	addButton,
 	subtractButton,
@@ -19,22 +31,23 @@ setOperatorEventListener(
 	divideButton,
 );
 
-calculateButton.addEventListener("click", calculate);
-
-number20.addEventListener("click", (e) => {
-	numberStore.push(e.target.value);
-	console.log(number);
-});
 
 // push operator found in the button's value
-function addOperatorToNumber(operatorButton) {
+function pushOperator(operatorButton) {
 	if (numberStore[0]) {
 		numberStore.push(operatorButton.target.value);
 		console.log(numberStore);
 	}
 }
+
+function pushNumber(number) {
+	numberStore.push(number.target.value);
+	console.log(numberStore);
+}
+
 function calculate() {
 	const numberString = numberStore.join(" ");
 	const finalAnswers = eval(numberString);
+	numberStore = [];
 	console.log(finalAnswers);
 }
