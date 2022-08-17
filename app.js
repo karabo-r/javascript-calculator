978937669c28857162167a0ca403b3077d34f9ee
Karabo <karaborabothata0@gmail.com>
2022-08-17 19:19:07 +0200

    combine previous number to last indexed number

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const display = document.getElementById("display");
 const addButton = document.getElementById("addButton");
 const subtractButton = document.getElementById("subtractButton");
 const multiplyButton = document.getElementById("multiplyButton");
@@ -31,23 +32,43 @@ setOperatorEventListener(
 	divideButton,
 );
 
-
 // push operator found in the button's value
 function pushOperator(operatorButton) {
 	if (numberStore[0]) {
 		numberStore.push(operatorButton.target.value);
-		console.log(numberStore);
+		const numberString = numberStore.join(" ");
+		updateDisplay(numberString)
 	}
 }
 
 function pushNumber(number) {
-	numberStore.push(number.target.value);
+	numberStore.push(Number(number.target.value));
 	console.log(numberStore);
+	checkPreviousNumberAndReplace();
 }
 
 function calculate() {
-	const numberString = numberStore.join(" ");
-	const finalAnswers = eval(numberString);
+	if (numberStore) {
+		const numberString = numberStore.join(" ");
+		const finalAnswers = eval(numberString);
+		updateDisplay(finalAnswers);
+	}
 	numberStore = [];
-	console.log(finalAnswers);
+}
+
+function checkPreviousNumberAndReplace() {
+	const lastNumberIndex = numberStore.length - 1;
+	const previousNumberIndex = numberStore.length - 2;
+	const lastNumber = numberStore[lastNumberIndex];
+	const previousNumber = numberStore[previousNumberIndex];
+	if (typeof lastNumber === "number" && typeof previousNumber === "number") {
+		const newNumber = Number(previousNumber.toString() + lastNumber.toString());
+		numberStore.splice(previousNumberIndex, 2, newNumber);
+	}
+	const numberString = numberStore.join(" ");
+	updateDisplay(numberString);
+}
+
+function updateDisplay(data) {
+	display.value = data;
 }
